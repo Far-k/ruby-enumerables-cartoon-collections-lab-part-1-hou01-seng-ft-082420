beff4ae824902824cb8a0fef15777caff6e5ac34
Farzana Karim <farzana.mkarim@gmail.com>
2020-07-27 19:52:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,18 @@
 def greet_characters(array)
+  array.each { |i|
+        puts "Hello #{i}!" }
+  end
+
+
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
-end
+
 
 def list_dwarves(array)
+  
+  end
+
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
-end
